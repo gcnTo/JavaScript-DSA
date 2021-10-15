9163646f34b3d6f5c833e452dd652e3fc152f25f
gcnTo <60984509+gcnTo@users.noreply.github.com>
2021-10-15 15:51:22 +0300

    Create hashtable.js

---
+++ hashtable.js
@@ -0,0 +1,18 @@
+function doesArraysContainSameElement(arr1,arr2){
+  //Creates an object called dictionary
+  dictionary = {};
+  //Turns arr1 into a hashtable(dictionary)
+  for (let i=0; i < arr1.length; i++){
+    list[arr1[i]] = true;
+  }
+  //Checks if any item in arr2 is also in the dictionary
+  for (let j=0; j < arr2.length; j++){
+    if (list[arr2[j]] === true){
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(doesArraysContainSameElement([1,2,3],[3,5,6])) // Should return true
+console.log(doesArraysContainSameElement([1,2,3],[4,5,6])) // Should return false
